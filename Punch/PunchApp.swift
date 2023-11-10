8386e379fe5c895721eb73a37596c5a4bc2b556b
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2023-11-10 16:43:48 -0500

    Initial Commit

---
+++ Punch/PunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchApp.swift
+//  Punch
+//
+//  Created by Ahmed Nasr on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
